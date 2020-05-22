9bde40ff0971ebacb5b31227a00dcdda20460deb
Matei <info.claudiu@gmail.com>
2020-05-22 15:51:13 +0200

    swap skills flow right/left

--- src/components/skills.js
+++ src/components/skills.js
@@ -7,27 +7,27 @@ export const Skills = ({skillsSet}) =>(
   <div id="skills">
     <SectionTitle title={"Skills"}/>
     <div id="backend" className="skillBlock">
-      <TwoBlockLeft skillsSet={skillsSet.edges.filter(({ node }) => node.type === "backend")[0].node}
+      <TwoBlockRight skillsSet={skillsSet.edges.filter(({ node }) => node.type === "backend")[0].node}
                     color={"#424555"}
                     image={require("../images/backend-b.png")}/>
     </div>
     <div id="frontend" className="skillBlock">
-      <TwoBlockRight skillsSet={skillsSet.edges.filter(({ node }) => node.type === "frontend")[0].node}
+      <TwoBlockLeft skillsSet={skillsSet.edges.filter(({ node }) => node.type === "frontend")[0].node}
                      color={"#007a80"}
                      image={require("../images/frontend-b.png")}/>
     </div>
     <div id="website" className="skillBlock">
-      <TwoBlockLeft skillsSet={skillsSet.edges.filter(({ node }) => node.type === "website")[0].node}
+      <TwoBlockRight skillsSet={skillsSet.edges.filter(({ node }) => node.type === "website")[0].node}
                     color={"#d97578"}
                     image={require("../images/website-b.png")}/>
     </div>
     <div id="design" className="skillBlock">
-      <TwoBlockRight skillsSet={skillsSet.edges.filter(({ node }) => node.type === "design")[0].node}
+      <TwoBlockLeft skillsSet={skillsSet.edges.filter(({ node }) => node.type === "design")[0].node}
                      color={"#424555"}
                      image={require("../images/design-b.png")}/>
     </div>
     <div id="other" className="skillBlock">
-      <TwoBlockLeft skillsSet={skillsSet.edges.filter(({ node }) => node.type === "other")[0].node}
+      <TwoBlockRight skillsSet={skillsSet.edges.filter(({ node }) => node.type === "other")[0].node}
                     color={"#007a80"}
                     image={require("../images/mouth-b.png")}/>
     </div>
