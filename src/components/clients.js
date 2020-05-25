e29332d8ad9c3b6b01530f97a0d04feebe7de15a
Matei <info.claudiu@gmail.com>
2020-05-25 11:16:48 +0200

    Add alt to img

--- src/components/clients.js
+++ src/components/clients.js
@@ -31,25 +31,25 @@ export const Clients = () => (
 
     <FlexContainerStyled>
       <FlexItemStyled>
-        <img src={require("../images/clients/dpg-media.svg")}/>
+        <img src={require("../images/clients/dpg-media.svg")} alt="DPG Media"/>
       </FlexItemStyled>
       <FlexItemStyled>
-        <img src={require("../images/clients/nobel-biocare.png")}/>
+        <img src={require("../images/clients/nobel-biocare.png")} alt="Nobel Biocare"/>
       </FlexItemStyled>
       <FlexItemStyled>
-        <img src={require("../images/clients/logo-cheops.svg")} style={{height:'3rem'}}/>
+        <img src={require("../images/clients/logo-cheops.svg")} style={{height:'3rem'}} alt="Cheops"/>
       </FlexItemStyled>
       <FlexItemStyled>
-        <img src={require("../images/clients/ABVV-FGTB.png")}/>
+        <img src={require("../images/clients/ABVV-FGTB.png")} alt="ABVV-FGTB"/>
       </FlexItemStyled>
       <FlexItemStyled>
-        <img src={require("../images/clients/digipolis.png")}/>
+        <img src={require("../images/clients/digipolis.png")} alt="Digipolis"/>
       </FlexItemStyled>
       <FlexItemStyled>
-        <img src={require("../images/clients/cronos-groep.png")}/>
+        <img src={require("../images/clients/cronos-groep.png")} alt="Cronos"/>
       </FlexItemStyled>
       <FlexItemStyled>
-        <img src={require("../images/clients/de-juristen.jpg")} style={{height:'7rem'}}/>
+        <img src={require("../images/clients/de-juristen.jpg")} style={{height:'7rem'}} alt="De Juristen"/>
       </FlexItemStyled>
     </FlexContainerStyled>
   </div>
