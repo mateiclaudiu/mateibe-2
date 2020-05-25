a5b7c277b0a703374a84005a232ee0cd9ab66a12
Matei <info.claudiu@gmail.com>
2020-05-25 11:42:29 +0200

    Page not found text

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,11 +4,10 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { SectionTitle } from "../components/section-title"
 
-const bibleVerse = "Hij geeft mij nieuwe kracht en leidt mij langs veilige paden tot eer van zijn naam. - Psalm 23:3"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found"/>
-    <SectionTitle title={"OEPS. PAGINA NIET GEVONDEN"} subtitle={bibleVerse}/>
+    <SectionTitle title={"OH 404, PAGE NOT FOUND BUDDY"} />
   </Layout>
 )
 
