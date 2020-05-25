ecbc2aa9c43a8491aaf79cec4b97441a9fafc145
Matei <info.claudiu@gmail.com>
2020-05-25 13:30:59 +0200

    Intro text first attempt

--- src/components/hello.js
+++ src/components/hello.js
@@ -5,10 +5,16 @@ import { SectionTitle } from "./section-title"
 export const Hello = () => (
   <div>
     <SectionTitle title={"Hello & Welcome"} subtitle={"A small introduction"}/>
+    <div style={{ fontFamily: "Poppins" }}>
+      I am a software developer, with over four years of experience as a consultant for different big organisations in Belgium.
+      <br/>
+      <br/>
+      I can help you with creating your software, web application and/or website.
+    </div>
     <ColumnStyled>
-      <div>I will introduce myself a bit later</div>
+      <div>
+      </div>
       <div style={{ marginTop: `20px` }}>
-        I am still thinking what I should tell you about myself...
       </div>
     </ColumnStyled>
   </div>
