d17621c0617e59aefea51447cdf4dd9625ddb5e7
Matei <info.claudiu@gmail.com>
2021-09-17 19:32:33 +0200

    update skillsset

--- src/components/hello.js
+++ src/components/hello.js
@@ -6,14 +6,9 @@ export const Hello = () => (
   <div id="hello">
     <SectionTitle title={"Hello & Welcome"} subtitle={"A small introduction"}/>
     <div style={{ fontFamily: "Poppins" }}>
-      Hey! Nice to have you here. Let me introduce myself. I am Claudiu Matei, a freelance software developer with over five years of experience as a consultant for different big organisations in Belgium like DPG Media, Nobel Biocare, ABVV etc.
-      I like to work on front as backend projects, where there is focus on good design and architecture.
-
+      Matei is small and dynamic IT company with a strong team specialized in developing web applications, mobile apps and websites.
+      We are stongly focused on quality, open communication and attractive designs.
       <br/>
-      <br/>
-      <b>
-        So how I could help you? I can help you with creating your custom software, web application, mobile application and website.
-      </b>
     </div>
     <ColumnStyled>
       <div>
